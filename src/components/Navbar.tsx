f4c674e7814c621acdbb83a68d64d71d815c90ab
Harshad Manish Karle <168220301+harshad-k-135@users.noreply.github.com>
2025-10-04 13:58:01 +0530

    updated resume

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -56,14 +56,7 @@ const Navbar: React.FC = () => {
                   {item}
                 </button>
               ))}
-              <a
-                href="https://drive.google.com/file/d/19WGRhqhNWSNaD_nFhPzpMWdnz3ai2tPR/view?usp=drivesdk"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-gray-700 dark:text-gray-300 hover:text-indigo-600 dark:hover:text-indigo-400 px-3 py-2 font-medium transition-colors"
-              >
-                Resume
-              </a>
+              
             </div>
           </div>
           
@@ -93,7 +86,7 @@ const Navbar: React.FC = () => {
             </button>
           ))}
           <a
-            href="https://drive.google.com/file/d/1jjIQqvf4Q86Tphwhbky7FSIoqatMYNPm/view?usp=sharing"
+            href="https://drive.google.com/file/d/19WGRhqhNWSNaD_nFhPzpMWdnz3ai2tPR/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="block w-full text-left px-3 py-2 text-gray-700 dark:text-gray-300 hover:text-indigo-600 dark:hover:text-indigo-400 font-medium"
